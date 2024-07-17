c8cd951571c7b79ec1da9d30b65092a31d60dcc1
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2024-07-18 01:40:30 +0700

    Create authController.js

---
+++ server/controllers/authController.js
@@ -0,0 +1,28 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+// Register
+exports.register = async (req, res) => {
+    try {
+        const { username, password, email } = req.body;
+        const user = new User({ username, password, email });
+        await user.save();
+        res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+// Login
+exports.login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username, password });
+        if (!user) return res.status(400).json({ message: 'Invalid credentials' });
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.status(200).json({ token });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
