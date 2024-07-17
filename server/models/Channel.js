e67833dad55a5ce2746867abbbc60a25c85bdfab
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2024-07-18 01:16:09 +0700

    Create Channel.js

---
+++ server/models/Channel.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const channelSchema = new mongoose.Schema({
+    name: { type: String, required: true, unique: true },
+    users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    messages: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message' }]
+});
+
+module.exports = mongoose.model('Channel', channelSchema);
