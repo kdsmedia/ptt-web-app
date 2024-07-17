5c16fb7f10adac564a8b1a05252793ad940778ee
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2024-07-18 01:18:58 +0700

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use('/api', require('./routes/api'));
+app.use('/auth', require('./routes/auth'));
+app.use('/channel', require('./routes/channel'));
+app.use('/user', require('./routes/user'));
+
+// Static files
+app.use(express.static('public'));
+
+// Error handling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
