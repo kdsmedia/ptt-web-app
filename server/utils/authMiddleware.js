f0296f79bca67c2378069e8a0743d943f8c3d801
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2024-07-18 01:15:35 +0700

    Create authMiddleware.js

---
+++ server/utils/authMiddleware.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    jwt.verify(token, 'your_jwt_secret', (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+        req.user = decoded;
+        next();
+    });
+};
+
+module.exports = authMiddleware;
